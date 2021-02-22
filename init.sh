79d3d7952b67907e019f28325cdeaefdc6705e61
MatCyg <matcyg@gmail.com>
2021-02-22 17:52:34 +0100

    [SETUP] Update init.sh

--- init.sh
+++ init.sh
@@ -3,7 +3,9 @@
 echo "Install XCode CLI Tool"
 #xcode-select --install
 
-git clone git@github.com:MatCyg/dotfiles.git "$HOME"/test_install/personal/dotfiles && cd "$_" && ./mac_setup/mac_setup.sh
+TARGET_DIR="$HOME"/test_install/personal/dotfiles
+git clone git@github.com:MatCyg/dotfiles.git "$TARGET_DIR"
+"$TARGET_DIR"/mac_setup/mac_setup.sh
 
 
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/MatCyg/dotfiles/master/init.sh)"
