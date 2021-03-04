b688691cc3e42a9481abe01c3f7cf0fc2b3127d9
MatCyg <matcyg@gmail.com>
2021-03-04 18:00:31 +0100

    Make `sudo` use touchId when possible

--- mac_setup/defaults.sh
+++ mac_setup/defaults.sh
@@ -216,6 +216,13 @@ defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 chflags nohidden ~/Library
 
+# allow touch id instead of password for sudo:
+l="auth       sufficient     pam_tid.so"
+f="/etc/pam.d/sudo"
+t="/tmp/sudo"
+## Add the following as the first line
+#auth sufficient pam_tid.so
+grep -q pam_tid.so $f || (  echo "$l" > $t && cat "$f" >> $t && sudo mv $t $f  )
 
 killall Dock
 killall Finder
