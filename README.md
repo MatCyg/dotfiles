7d5922facc56df3b7bfc0975935353a67e99c7df
MatCyg <matcyg@gmail.com>
2021-03-04 17:30:12 +0100

    Add initialization script for automatic one line setup

---
+++ README.md
@@ -0,0 +1,4 @@
+To start automatic macOS setup run:
+```shell
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/MatCyg/dotfiles/master/init.sh)"
+```
--- init.sh
+++ init.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 
 echo "Install XCode CLI Tool"
-#xcode-select --install
+xcode-select --install
 
-TARGET_DIR="$HOME"/test_install/personal/dotfiles
-git clone git@github.com:MatCyg/dotfiles.git "$TARGET_DIR"
-"$TARGET_DIR"/mac_setup/mac_setup_test.sh
-
-
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/MatCyg/dotfiles/master/init.sh)"
-
+TARGET_DIR="$HOME"/Projects/personal/dotfiles
+git clone https://github.com/MatCyg/dotfiles.git "$TARGET_DIR"
+"$TARGET_DIR"/mac_setup/mac_setup.sh
