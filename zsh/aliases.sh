aba493fa25e9f020bea2b6f82d5733f0944cfef3
Mateusz Cygert <matcyg@gmail.com>
2023-08-02 22:29:30 +0200

    [zsh] Add dir aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -5,7 +5,13 @@ alias ....="cd ../../.."
 alias .....="cd ../../../.."
 alias -- -="cd -"
 alias cd..='cd ..'
-
+alias 1='cd -1'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias d='dirs -v'
 
 export PROJECTS=$HOME/Projects
 export DOWNLOADS=$HOME/Downloads
