5a7cec5adfa2b38813f78d6a49e224f7edbc33d0
Mateusz Cygert <matcyg@gmail.com>
2023-08-02 22:30:10 +0200

    [zsh] Setup dir history

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,6 +1,23 @@
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=129'
 export LANG="en_US.UTF-8"
+
 setopt SHARE_HISTORY HIST_IGNORE_DUPS
+setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
+
+autoload -U select-word-style
+select-word-style bash
+
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+
+# Enable the "new" completion system (compsys).
+autoload -Uz compinit
+compinit
+if [[ ! ~/.zcompdump.zwc -nt ~/.zcompdump ]]; then
+  zcompile -R -- ~/.zcompdump.zwc ~/.zcompdump
+fi
 
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
@@ -18,15 +35,3 @@ export SDKMAN_OFFLINE_MODE=true
 source ${zsh_plugins_zsh}
 
 unset SDKMAN_OFFLINE_MODE
-
-# Enable the "new" completion system (compsys).
-autoload -Uz compinit
-compinit
-if [[ ! ~/.zcompdump.zwc -nt ~/.zcompdump ]]; then
-  zcompile -R -- ~/.zcompdump.zwc ~/.zcompdump
-fi
-
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
-
-autoload -U select-word-style
-select-word-style bash
