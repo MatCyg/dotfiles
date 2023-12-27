08385d939786a81a8bac58aec60e1dd429263551
Mateusz Cygert <matcyg@gmail.com>
2023-12-27 22:29:40 +0100

    [zsh] Increase history size

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -1,6 +1,13 @@
 export LANG="en_US.UTF-8"
 
+export HISTFILESIZE=1000000000
+export HISTSIZE=100000
+export SAVEHIST=100000
+setopt INC_APPEND_HISTORY
+export HISTTIMEFORMAT="[%F %T] "
+setopt EXTENDED_HISTORY
 setopt SHARE_HISTORY HIST_IGNORE_DUPS
+
 setopt auto_cd
 setopt auto_pushd
 setopt pushd_ignore_dups
